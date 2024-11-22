e952798bb2ce93c38f64df04cfad485b0e44b487
Florecitaa <160565962+Florecitaa@users.noreply.github.com>
2024-11-21 18:44:14 -0600

    Creacion de controler,modelo persona y vista de log in

---
+++ ProyectoCondominios/Models/Persona.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoCondominios.Models
+{
+    public class Persona
+    {
+        
+            public int IdPersona { get; set; }
+            public string NombreCompleto { get; set; }
+            public string TipoPersona { get; set; }
+        
+    }
+}
