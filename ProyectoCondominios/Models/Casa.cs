5f89c4719a680bd34e26059f41a827868c091d4c
Florecitaa <160565962+Florecitaa@users.noreply.github.com>
2024-12-01 15:19:16 -0600

    Creacion de modelo casa y creacion de sp : USE [PVI_ProyectoFinal] GO ALTER TABLE Casas ALTER COLUMN nombre_casa NVARCHAR(MAX);
    USE [PVI_ProyectoFinal]
    GO
    CREATE OR ALTER PROCEDURE SP_GetCasas
    AS
    BEGIN
        SELECT c.id_casa, c.nombre_casa, c.metros_cuadrados, c.numero_habitaciones,
               c.numero_banos, p.nombre AS propietario,
               c.fecha_construccion, c.estado
        FROM Casas c
        JOIN Persona p ON c.id_persona = p.id_persona
        ORDER BY c.nombre_casa ASC;
    END;
    
    CREATE OR ALTER PROCEDURE SP_InsertCasa
        @nombre NVARCHAR(50),
        @metrosCuadrados DECIMAL(10,2),
        @numeroHabitaciones INT,
        @numeroBanos INT,
        @idPersona INT,
        @fechaConstruccion DATE,
        @estado BIT
    AS
    BEGIN
        INSERT INTO Casas (nombre_casa, metros_cuadrados, numero_habitaciones, numero_banos, id_persona, fecha_construccion, estado)
        VALUES (@nombre, @metrosCuadrados, @numeroHabitaciones, @numeroBanos, @idPersona, @fechaConstruccion, @estado);
    END;
    
    CREATE PROCEDURE sp_InactivateCasa
        @id INT
    AS
    BEGIN
        UPDATE Casas
        SET estado = 0
        WHERE id_casa = @id;

---
+++ ProyectoCondominios/Models/Casa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoCondominios.Models
+{
+    public class Casa
+    {
+        //datos de la casa
+        public int IdCasa { get; set; }
+        public string NombreCasa { get; set; }
+        public decimal MetrosCuadrados { get; set; }
+        public int NumeroHabitaciones { get; set; }
+        public int NumeroBanos { get; set; }
+        public int IdPersona { get; set; }
+        public DateTime FechaConstruccion { get; set; }
+        public bool Estado { get; set; } // true = Activa, false = Inactiva
+
+    }
+}
