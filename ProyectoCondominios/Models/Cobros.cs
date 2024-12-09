2497c493d0bb6621ec34dd8043facbb5247fd276
kevin030799 <42698483+kevin030799@users.noreply.github.com>
2024-12-08 23:21:56 -0600

    Add files via upload

---
+++ ProyectoCondominios/Models/Cobros.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoCondominios.Models
+{
+    public class Cobros
+    {
+        public int Id_cobro { get; set; }
+
+        public int Id_Casa { get; set; }
+
+        public string Nombre_casa { get; set; }
+
+        public decimal Monto { get; set; }
+
+        public string Nombre { get; set; }
+
+        public decimal Precio_casa { get; set; }
+
+        public int Periodo { get; set; }
+
+        public bool Estado { get; set; } //Pendiente o Pagado
+
+        public bool fecha { get; set; }
+
+        public int Mes { get; set; }
+
+        public int Año { get; set; }
+    }
+}
